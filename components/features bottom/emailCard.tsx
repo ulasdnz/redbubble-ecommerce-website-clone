a0584e9ac6a0f33d37d8c49f016ac73685a14f7a
ulasdnz <ulasdenizisik@gmail.com>
2023-09-19 09:46:14 +0300

    add features bottom section to home page

--- components/features bottom/emailCard.tsx
+++ components/features bottom/emailCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import TagSvg from "../../public/home/gray section/tags.svg";
 import Image from "next/image";
+import Arrow from "../../public/home/gray section/arrow.svg";
 
 const emailCard = () => {
   return (
@@ -15,10 +16,43 @@ const emailCard = () => {
             alt="Tags svg icon"
           />
         </div>
-        <div className="font-semibold ml-4 w-[238px]">10% off, promos, and the best indie art ever! Your Email</div>
+        <div className="font-semibold ml-4 w-[238px]">
+          10% off, promos, and the best indie art ever! Your Email
+        </div>
       </div>
-      <div></div>
-      <div></div>
+      <div className="w-full mt-3 relative">
+        <input
+          type="text"
+          className="text-sm text-[#19124F] w-full h-10 border-[#8C95A5] border-solid border-b-2
+         pl-[10px] pr-10 placeholder:text-[#5A5A5A]"
+          placeholder="Your email address"
+        />
+
+        <div className="absolute top-2 right-1">
+          <Image
+            src={Arrow}
+            alt="arrow send email icon"
+            width={28}
+            height={38}
+            className="py-[1px] px-[6px] "
+          />
+        </div>
+      </div>
+
+      <div className=" w-[319pxpx] my-[11px] pl-3">
+        <p className="text-[11px] text-[#757195] w-[330px]"
+        >
+          By submitting, you agree to our  &nbsp;
+          <a
+            className="text-[#757195] font-bold w-[330px]"
+            href="https://www.redbubble.com/privacy"
+            target="_blank"
+          >
+            Privacy Policy &nbsp;
+          </a>
+           and to receive our promotional emails (opt out any time).
+        </p>
+      </div>
     </div>
   );
 };
--- components/features bottom/promotion.tsx
+++ components/features bottom/promotion.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import EmailCard from "./emailCard";
 const promotion = () => {
-  return <div className=" max-w-[1148px] w-full mr-auto ml-auto">
-    <EmailCard />
-  </div>;
+  return (
+    <div>
+      <div className=" max-w-[1148px] flex flex-col justify-center items-center w-full p-12 mr-auto ml-auto">
+        <EmailCard />
+      </div>
+      <div className="h-[5px] w-full bg-[#F2F2F6]"></div>
+      <div className="max-w-[1180px] px-6 py-8 flex justify-end mr-auto ml-auto">
+        <div className="mr-3 text-[18px]	font-extrabold">TR</div>
+        <div className="mr-6 font-semibold">USD$ - English</div>
+        <div className="font-semibold">Mature content: Hidden</div>
+      </div>
+    </div>
+  );
 };
 
 export default promotion;
